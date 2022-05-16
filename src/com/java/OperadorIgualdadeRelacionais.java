0b133ca5cb7c391154146f5e2eb2d7fde776bfd7
Marco Aurelio <marco.apires91@gmail.com>
2022-05-16 15:59:11 -0300

    Operadores de igualdade relacional

---
+++ src/com/java/OperadorIgualdadeRelacionais.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class OperadorIgualdadeRelacionais {
+    public static void main(String[] args) {
+        //int idade = 10;
+        //boolean maioridade = idade > 18;
+
+        //int idade = 20;
+        //boolean maioridade = idade == 18; //usado para fazer comparação se a variável e o valor são iguais
+
+        int idade = 20;
+        boolean maioridade = idade != 18; //usado para fazer comparação se a variável e o valor são diferentes
+
+
+
+
+
+        System.out.println(maioridade);
+    }
+}
